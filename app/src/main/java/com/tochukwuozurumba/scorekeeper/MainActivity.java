23eb6b0596087189ee5113a34011647585e8dd54
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2022-07-20 16:25:22 -0400

    Score Keeper layout complete

---
+++ app/src/main/java/com/tochukwuozurumba/scorekeeper/MainActivity.java
@@ -0,0 +1,17 @@
+package com.tochukwuozurumba.scorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RadioGroup rg = findViewById(R.id.radio_groups);
+
+    }
+}
