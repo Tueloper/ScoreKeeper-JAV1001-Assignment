40b214b4dea9873243be4078b2ffcc23c43610fb
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2022-07-20 18:36:58 -0400

    Styles are configured and in progress

--- app/src/main/java/com/tochukwuozurumba/scorekeeper/MainActivity.java
+++ app/src/main/java/com/tochukwuozurumba/scorekeeper/MainActivity.java
@@ -1,6 +1,6 @@
 package com.tochukwuozurumba.scorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RadioGroup;
 
